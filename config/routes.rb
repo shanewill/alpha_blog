554645923b8811ea0157a53936ca98ccc14ef938
shanem <shane.means@procore.com>
2018-01-22 13:12:11 -0800

    added users index view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+  resources :users, except:[:new]
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
