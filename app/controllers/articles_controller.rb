4524bc39254f13a055df70197490b80ffa465375
shanem <shane.means@procore.com>
2018-01-23 13:34:26 -0800

    added admin role

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.users
+    if current_user != @article.users && !current_user.admin?
       flash[:danger] = "You can only edit or delete your own article"
       redirect_to root_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118231532) do
+ActiveRecord::Schema.define(version: 20180123184305) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180118231532) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
