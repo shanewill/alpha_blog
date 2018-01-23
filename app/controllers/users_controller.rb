0716df196fa2ffb17d8cbb4a91fce96d693023b9
shanem <shane.means@procore.com>
2018-01-23 10:12:35 -0800

    signed in user after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:sucess] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -49,4 +50,5 @@ class UsersController < ApplicationController
     if current_user != @user
       flash[:danger] = "You can only edit your own account"
     end
+  end
 end
